4060fc596af614890f581f0967de8a344840d078
EmptyRamenBox <EmptyRamenBox.alexis.martinezo92@gmail.com>
2020-03-13 20:11:47 -0500

    Added Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
-  <h1>Hello There!</h1>
+  <div class="mt-4 flex flex-col justify-center items-center">
+    <UndrawWelcome primary-color="#4c51bf" height="24em" />
+    <h1 class="text-6xl font-hairline text-indigo-900">Welcome</h1>
+    <p class="text-blue-900 text-xl tracking-widest">
+      Simple example of how to register and access a website, there is a public
+      and private page.
+    </p>
+  </div>
 </template>
 
 <script>
+import UndrawWelcome from "vue-undraw/UndrawWelcome";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    UndrawWelcome
+  }
 };
 </script>
