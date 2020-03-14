3c12a0988ba9b12da8329fce4c72fd603c502a14
EmptyRamenBox <EmptyRamenBox.alexis.martinezo92@gmail.com>
2020-03-13 22:03:12 -0500

    Fixed broken login system Incorrect Login State

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,45 +1,29 @@
 <template>
-  <nav
-    class="flex 
-    bg-indigo-700 text-white 
-    items-center h-16 px-8 justify-between"
-  >
+  <nav class="flex bg-indigo-700 text-white items-center h-16 px-8 justify-between">
     <div>
-      <router-link class="logo font-bold text-xl tracking-tight" to="/"
-        >The Login</router-link
-      >
+      <router-link class="logo font-bold text-xl tracking-tight" to="/">TheLogin</router-link>
     </div>
     <div>
-      <span v-if="user" class="text-white font-bold mr-8"
-        >Welcome
-        <span class="font-normal">
-          {{ user.displayName }}
-        </span></span
-      >
+      <span v-if="user" class="text-white font-bold mr-8">
+        Welcome
+        <span class="font-normal">{{ user.displayName }}</span>
+      </span>
       <router-link
         v-if="!user"
         class="p-2 mx-1 hover:text-indigo-700 hover:bg-white"
         to="/login"
-        >Login</router-link
-      >
+      >Login</router-link>
       <button
         v-if="user"
         class="p-2 mx-1 hover:text-indigo-700 hover:bg-white"
         @click="logOut"
-      >
-        Logout
-      </button>
-      <router-link
-        class="p-2 mx-1 hover:text-indigo-700 hover:bg-white"
-        to="/public"
-        >Public</router-link
-      >
+      >Logout</button>
+      <router-link class="p-2 mx-1 hover:text-indigo-700 hover:bg-white" to="/public">Public</router-link>
       <router-link
         v-if="user"
         class="p-2 mx-1 hover:text-indigo-700 hover:bg-white"
         to="/private"
-        >Private</router-link
-      >
+      >Private</router-link>
     </div>
   </nav>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: {
-      displayName: ""
-    }
+    user: ''
   },
   getters: {
     getUser: state => state.user
